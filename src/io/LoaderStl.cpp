53a78dd3d01da51dbb1bec707f2dd99d36ff135b
mcamara <maxi23032000@gmail.com>
2025-09-14 20:30:20 -0300

    LoaderStl implementado con exito, pero necesita embellecimiento

--- src/io/LoaderStl.cpp
+++ src/io/LoaderStl.cpp
@@ -44,6 +44,9 @@
 #include "wrl/Material.hpp"
 #include "wrl/IndexedFaceSet.hpp"
 
+#include "map"
+#include "tuple" //Diccionarios y tuplas usados para el manejo de vectores repetidos
+
 // reference
 // https://en.wikipedia.org/wiki/STL_(file_format)
 
@@ -75,13 +78,24 @@ bool LoaderStl::load(const char* filename, SceneGraph& wrl) {
 
       // create the scene graph structure :
       // 1) the SceneGraph should have a single Shape node a child
+      Shape* shape = new Shape();
+      wrl.addChild(shape);
+
       // 2) the Shape node should have an Appearance node in its appearance field
+      Appearance* appear = new Appearance();
+      shape->setAppearance(appear);
+
       // 3) the Appearance node should have a Material node in its material field
+      Material* material = new Material();
+      appear->setMaterial(material);
+
       // 4) the Shape node should have an IndexedFaceSet node in its geometry node
+      IndexedFaceSet* ifs = new IndexedFaceSet();
+      shape->setGeometry(ifs);
 
       // from the IndexedFaceSet
       // 5) get references to the coordIndex, coord, and normal arrays
-      // 6) set the normalPerVertex variable to false (i.e., normals per face)  
+      // 6) set the normalPerVertex variable to false (i.e., normals per face)
 
       // the file should contain a list of triangles in the following format
 
@@ -101,6 +115,55 @@ bool LoaderStl::load(const char* filename, SceneGraph& wrl) {
       // - if your method returns false
       //     throw an StrException explaining why the method failed
 
+      ifs->setNormalPerVertex(false);
+      int vertexIndex = 0;
+      vector<float>& _coord = ifs->getCoord();
+      vector<int>& _coordIndex = ifs->getCoordIndex();
+      vector<float>& _normal = ifs->getNormal();
+
+      map<tuple<float,float,float>, int> vertexMap;
+
+      //while(!(tkn.expecting("endsolid"))){
+      while(tkn.get()){
+          if(!(tkn.equals("facet"))) throw new StrException("Expecting 'facet' token");
+          tkn.get("Expecting 'normal' token");
+
+          float coordN1, coordN2, coordN3;
+          tkn.getFloat(coordN1);
+          tkn.getFloat(coordN2);
+          tkn.getFloat(coordN3);
+          _normal.push_back(coordN1);
+          _normal.push_back(coordN2);
+          _normal.push_back(coordN3);
+
+          tkn.get("Expecting 'outer' token");
+          tkn.get("Expecting 'loop' token");
+
+          while(!(tkn.expecting("endloop"))){
+              if(!(tkn.equals("vertex"))) throw new StrException("Expecting 'vertex' token");
+
+              float coordV1, coordV2, coordV3;
+              tkn.getFloat(coordV1);
+              tkn.getFloat(coordV2);
+              tkn.getFloat(coordV3);
+
+              tuple<float,float,float> vertex = make_tuple(coordV1, coordV2, coordV3);
+              if(vertexMap.count(vertex) == 1){
+                  _coordIndex.push_back(vertexMap[vertex]);
+              } else {
+                  _coord.push_back(coordV1);
+                  _coord.push_back(coordV2);
+                  _coord.push_back(coordV3);
+                  _coordIndex.push_back(vertexIndex);
+                  vertexMap[vertex] = vertexIndex;
+                  vertexIndex++;
+              }
+          }
+          tkn.get("endfacet");
+          _coordIndex.push_back(-1); //Ya agregué todos los vértices correspondientes a una cara
+      }
+      success = true;
+      //tkn.get("Expecting final name token");
     }
 
     // close the file (this statement may not be reached)
